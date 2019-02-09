530fed0992463df6dbbeb431fd420216fe2e5ba7
agseijas <andresgseijas@gmail.com>
2019-02-09 22:34:11 +0100

    first two rules implemented

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,50 +1,25 @@
-
-it('world is created dead', () => {
-  var world = [
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0]
-  ];
-  expect(world).toHaveLength(4);
-  expect(world[0]).toHaveLength(8);
-  world.map(line => {
-    line.map(cell => {
-      expect(cell).toBe(0);
-    })
-  })
-});
-
-it('world has some cell alive', () => {
-  var world = [
-    [0, 0, 1, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 1, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 1, 0, 0, 0, 0]
-  ];
-
-  expect(world[0][2]).toBe(1)
-});
-
 it('we get expected neighbours', () => {
   var world = [
     [0, 0, 1, 0, 1, 1, 0, 0],
     [0, 0, 0, 0, 0, 1, 1, 0],
-    [0, 0, 0, 0, 0, 0, 1, 0],
-    [0, 0, 0, 1, 0, 0, 0, 0]
+    [0, 1, 0, 0, 0, 0, 1, 1],
+    [0, 1, 0, 1, 0, 0, 1, 0]
   ];
 
   let neighbours = getNeighboursForCell(world, 1, 5);
   expect(neighbours).toEqual([1, 1, 0, 0, 1, 0, 0, 1])
 
-
   neighbours = getNeighboursForCell(world, 0, 0);
   expect(neighbours).toEqual([0, 0, 0, 0, 0, 0, 0, 0])
 
-
   neighbours = getNeighboursForCell(world, 3, 7);
-  expect(neighbours).toEqual([1, 0, 0, 0, 0, 0, 0, 0])
+  expect(neighbours).toEqual([1, 1, 0, 1, 0, 0, 0, 0])
 
+  neighbours = getNeighboursForCell(world, 3, 0);
+  expect(neighbours).toEqual([0, 0, 1, 0, 1, 0, 0, 0])
+
+  neighbours = getNeighboursForCell(world, 0, 7);
+  expect(neighbours).toEqual([0, 0, 0, 0, 0, 1, 0, 0])
 });
 
 it('counts live neighbours', () => {
@@ -58,6 +33,50 @@ it('counts live neighbours', () => {
 
   expect(countAliveNeighboursForCell(world, 1, 5)).toBe(4);
 });
+
+it('lone cell dies after one beat', () => {
+  var world = [
+    [0, 1]
+  ];
+
+  let newWorld = beat(world);
+
+  expect(newWorld).toEqual([[0,0]])
+});
+
+it('cell in overpopulation dies after one beat', () => {
+  var world = [
+    [1, 1, 0],
+    [1, 1, 1]
+  ];
+
+  let newWorld = beat(world);
+
+  expect(newWorld).toEqual([[1, 0, 0], [1, 0, 1]])
+});
+
+function beat(world: number[][]) {
+  const newWorld = cloneArray(world)
+  world.map((line, x) => {
+    line.map((cell, y) => {
+      let numAliveCells = countAliveNeighboursForCell(world, x, y)
+
+      if(numAliveCells < 2 || numAliveCells > 3){
+        newWorld[x][y] = 0
+      }
+    });
+  });
+  return newWorld
+}
+
+function cloneArray(world: number[][]) {  
+  var clone = [];
+  for (let i=0; i<world.length; i++) {
+    clone.push( world[i].slice(0) )
+  }
+  return clone
+}
+ 
 
 function countAliveNeighboursForCell(world: number[][], posx: number, posy: number) {
   return getNeighboursForCell(world, posx, posy).filter(cell => cell == 1).length
@@ -86,7 +105,7 @@ function getNeighboursForCell(world: number[][], posx: number, posy: number): nu
   if (posx < world.length - 1) {
     neighbours[6] = world[posx + 1][posy];
   }
-  if (posx < world.length - 1) {
+  if (posx < world.length - 1 && posy < world[posx].length - 1) {
     neighbours[7] = world[posx + 1][posy + 1];
   }
   return neighbours;
